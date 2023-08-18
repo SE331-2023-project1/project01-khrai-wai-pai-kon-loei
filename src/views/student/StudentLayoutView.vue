a381afd1c374112706e7bba423cd2ef3772e607c
Pattanachai_n <pondlala33@gmail.com>
2023-08-18 23:15:36 +0700

    edit comment box

--- src/views/student/StudentLayoutView.vue
+++ src/views/student/StudentLayoutView.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import type { Ref } from "vue";
-import type { PropType } from "vue";
 import type { Student } from "@/type";
 import StudentService from "@/services/StudentService";
-import StudentCard from "@/components/StudentCard.vue";
-import Vue from "vue";
 
 const students = ref<Student | null>(null);
 
@@ -39,44 +35,53 @@ function deleteComment(index: number) {
 
 <template>
   <main class="container">
-    <!-- <StudentCard v-for="student in students" :key="student.studentid" :student="student"></StudentCard> -->
     <div v-if="students">
-      <img
-        class="w-24 h-24 object-cover mb-3 rounded-full shadow-lg"
-        :src="students?.profileimage"
-      />
-      <h1>Name: {{ students.name }} {{ students.surname }}</h1>
-      <h1>Studentid: {{ students.studentid }}</h1>
-      <p>Courselist: {{ students.courselist }}</p>
-      <p>Comment:</p>
-      <p>{{ students.comment }}</p>
-
-      <div>
-        <div class="mt-4">
-          <div
-            v-for="(comment, index) in comments"
-            :key="index"
-            class="bg-gray-100 p-2 rounded-lg"
-          >
-            {{ comment }}
-            <button @click="deleteComment(index)" class="ml-2 text-red-500">
-              Delete
-            </button>
+      <div class="bg-white p-6 rounded-lg shadow-md space-y-4">
+        <div class="flex items-center space-x-4">
+          <img
+            class="w-24 h-24 object-cover rounded-full shadow-lg"
+            :src="students?.profileimage"
+          />
+          <div>
+            <h1 class="text-2xl font-semibold">
+              {{ students.name }} {{ students.surname }}
+            </h1>
+            <h1 class="text-lg">{{ students.studentid }}</h1>
+            <h1 class="text-lg">{{ students.courselist }}</h1>
+            <h1 class="text-lg">{{ students.teacherID }}</h1>
           </div>
         </div>
 
-        <div class="comment-box p-4 bg-white shadow-md rounded-lg">
-          <textarea
-            v-model="newComment"
-            placeholder="Write a comment"
-            class="w-full p-2 border rounded focus:outline-none focus:ring focus:border-blue-500"
-          ></textarea>
-          <button
-            @click="submitComment"
-            class="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300 ease-in-out"
-          >
-            Submit
-          </button>
+        <div class="border-t border-gray-300 pt-4">
+          <h2 class="text-lg font-semibold">Comment</h2>
+          <p class="mb-2">{{ students.comment }}</p>
+
+          <div class="mt-4">
+            <div
+              v-for="(comment, index) in comments"
+              :key="index"
+              class="bg-gray-100 p-2 rounded-lg mb-2 flex justify-between items-center"
+            >
+              <span>{{ comment }}</span>
+              <button @click="deleteComment(index)" class="text-red-500">
+                Delete
+              </button>
+            </div>
+          </div>
+
+          <div class="comment-box mt-4">
+            <textarea
+              v-model="newComment"
+              placeholder="Write a comment"
+              class="w-full p-2 border rounded focus:outline-none focus:ring focus:border-blue-500"
+            ></textarea>
+            <button
+              @click="submitComment"
+              class="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300 ease-in-out"
+            >
+              Submit
+            </button>
+          </div>
         </div>
       </div>
     </div>
