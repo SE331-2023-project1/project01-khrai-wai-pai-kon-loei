d5cf3bc19f9dcbdf0d906169f3685ce8c456cb71
danaikrit <danaikrit_j@cmu.ac.th>
2023-08-20 19:45:04 +0700

    add cloud animation

--- src/views/ContrinutorsListView.vue
+++ src/views/ContrinutorsListView.vue
@@ -30,12 +30,136 @@ watchEffect(() => {
 
 
 <template>
+  <div class="moon"></div>
+  <div class="cloud">
+    <div class="blush"></div>
+    <div class="eye-l"></div>
+    <div class="eye-r"></div>
+    <div class="mouth"></div>
+  </div>
   <main class="container">
     <ContributorsCard  v-for="contributor in contributors" :key="contributor.studentId" :contributor="contributor"></ContributorsCard>
   </main>
 </template>
 
 <style scoped>
+.cloud {
+  background-color: #f5f5f5;
+  height: 6.25em;
+  width: 20.62em;
+  position: absolute;
+  margin: auto;
+  left: 1350px;
+  top: 9.75em;
+  border-radius: 6.25em;
+}
+
+.cloud:before {
+  position: absolute;
+  content: "";
+  height: 10.62em;
+  width: 10.62em;
+  background-color: #f5f5f5;
+  border-radius: 50%;
+  bottom: 1.87em;
+  left: 2.5em;
+}
+
+.cloud:after {
+  position: absolute;
+  content: "";
+  height: 6.25em;
+  width: 6.25em;
+  background-color: #f5f5f5;
+  border-radius: 50%;
+  bottom: 4em;
+  left: 11em;
+  z-index: -1;
+}
+
+.moon {
+  background-color: #787470;
+  height: 9.37em;
+  width: 9.37em;
+  border-radius: 50%;
+  position: absolute;
+  right: 17em;
+  top: 2.25em;
+  overflow: hidden;
+  z-index: -2;
+  animation: spin-moon 5s infinite linear;
+}
+@keyframes spin-moon {
+  100%{
+    transform: rotate(360deg);
+  }
+}
+
+.moon:before {
+  position: absolute;
+  content: "";
+  height: 1.87em;
+  width: 1.87em;
+  background-color: #565350;
+  border-radius: 50%;
+  top: 1.87em;
+  box-shadow: 5.62em 3.12em 0 0.75em #565350, 2.18em 4.68em 0 -0.3em #565350, 6.25em -1.87em 0 -0.18em #565350, 3.43em 0.3em 0 -0.5em #565350;
+}
+
+.blush {
+  height: 2.62em;
+  width: 2.62em;
+  background-color: #c9c9c9;
+  border-radius: 50%;
+  position: absolute;
+  left: 5em;
+  box-shadow: 6.87em 0 #c9c9c9;
+}
+
+.eye-l, .eye-r{
+  position: absolute;
+  height: 1.25em;
+  width: 2.12em;
+  background-color: #363636;
+  border-radius: 0 0 0.93em 0.93em;
+}
+
+.eye-l {
+  left: 6.87em;
+}
+
+.eye-r {
+  left: 11.25em;
+}
+
+.eye-l:after,.eye-r:after{
+  position: absolute;
+  content: "";
+  background-color: #f5f5f5;
+  height: 0.81em;
+  width: 1.25em;
+  border-radius: 0 0 0.93em 0.93em;
+  left: 0.43em;
+}
+
+.mouth {
+  height: 1.25em;
+  width: 1.25em;
+  background-color: #363636;
+  border-radius: 50%;
+  position: absolute;
+  left: 9.68em;
+  top: 1.56em;
+  animation: snoore 3s infinite;
+}
+
+@keyframes snoore {
+  50%{
+    transform: scale(1.3);
+  }
+}
+
+
 .container {
   margin: 3rem;
   display: grid;
